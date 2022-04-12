1cf99311ba771e8414e3bba247c8bd1134949515
Tuğra GÜNDÜZ <trgndz1999@gmail.com>
2022-04-12 18:56:17 +0300

    ListView with DataSource

---
+++ week09-1/week09-1/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace week09_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtItemTitle = new System.Windows.Forms.TextBox();
+            this.btnAddItem = new System.Windows.Forms.Button();
+            this.labelSelectedItem1 = new System.Windows.Forms.Label();
+            this.btnDeleteItem = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelSelectedItem2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "five",
+            "four",
+            "one",
+            "seven",
+            "six",
+            "three",
+            "two"});
+            this.listBox1.Location = new System.Drawing.Point(13, 85);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(100, 173);
+            this.listBox1.Sorted = true;
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Item Title";
+            // 
+            // txtItemTitle
+            // 
+            this.txtItemTitle.Location = new System.Drawing.Point(13, 30);
+            this.txtItemTitle.Name = "txtItemTitle";
+            this.txtItemTitle.Size = new System.Drawing.Size(100, 20);
+            this.txtItemTitle.TabIndex = 2;
+            // 
+            // btnAddItem
+            // 
+            this.btnAddItem.Location = new System.Drawing.Point(13, 56);
+            this.btnAddItem.Name = "btnAddItem";
+            this.btnAddItem.Size = new System.Drawing.Size(100, 23);
+            this.btnAddItem.TabIndex = 3;
+            this.btnAddItem.Text = "Add Item";
+            this.btnAddItem.UseVisualStyleBackColor = true;
+            this.btnAddItem.Click += new System.EventHandler(this.btnAddItem_Click);
+            // 
+            // labelSelectedItem1
+            // 
+            this.labelSelectedItem1.AutoSize = true;
+            this.labelSelectedItem1.Location = new System.Drawing.Point(13, 265);
+            this.labelSelectedItem1.Name = "labelSelectedItem1";
+            this.labelSelectedItem1.Size = new System.Drawing.Size(72, 13);
+            this.labelSelectedItem1.TabIndex = 4;
+            this.labelSelectedItem1.Text = "Selected Item";
+            // 
+            // btnDeleteItem
+            // 
+            this.btnDeleteItem.Location = new System.Drawing.Point(13, 281);
+            this.btnDeleteItem.Name = "btnDeleteItem";
+            this.btnDeleteItem.Size = new System.Drawing.Size(100, 23);
+            this.btnDeleteItem.TabIndex = 5;
+            this.btnDeleteItem.Text = "Delete Item";
+            this.btnDeleteItem.UseVisualStyleBackColor = true;
+            this.btnDeleteItem.Click += new System.EventHandler(this.btnDeleteItem_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Items.AddRange(new object[] {
+            "five",
+            "four",
+            "one",
+            "seven",
+            "six",
+            "three",
+            "two"});
+            this.listBox2.Location = new System.Drawing.Point(179, 85);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox2.Size = new System.Drawing.Size(148, 173);
+            this.listBox2.Sorted = true;
+            this.listBox2.TabIndex = 6;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(179, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Add Item Datasource";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelSelectedItem2
+            // 
+            this.labelSelectedItem2.AutoSize = true;
+            this.labelSelectedItem2.Location = new System.Drawing.Point(176, 265);
+            this.labelSelectedItem2.Name = "labelSelectedItem2";
+            this.labelSelectedItem2.Size = new System.Drawing.Size(72, 13);
+            this.labelSelectedItem2.TabIndex = 8;
+            this.labelSelectedItem2.Text = "Selected Item";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(179, 281);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Delete Item";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append;
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "DrawCircle",
+            "FillCircle"});
+            this.comboBox1.Location = new System.Drawing.Point(383, 58);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(170, 21);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(383, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(170, 170);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnAddItem;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.labelSelectedItem2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.btnDeleteItem);
+            this.Controls.Add(this.labelSelectedItem1);
+            this.Controls.Add(this.btnAddItem);
+            this.Controls.Add(this.txtItemTitle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "List Controls";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtItemTitle;
+        private System.Windows.Forms.Button btnAddItem;
+        private System.Windows.Forms.Label labelSelectedItem1;
+        private System.Windows.Forms.Button btnDeleteItem;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelSelectedItem2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ week09-1/week09-1/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace week09_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            listBox1.Items.Add("Horse");
+            listBox1.Items.Add("Hound");
+
+            btnDeleteItem.Enabled = false;
+        }
+
+        private void btnAddItem_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtItemTitle.Text.Trim()))
+                return;
+
+
+            bool found = false;
+            foreach (string item in listBox1.Items)
+            {
+                if (string.Compare(item, txtItemTitle.Text.Trim(), true) == 0)
+                    found = true;
+            }
+            if (found)
+                return;
+
+            listBox1.Items.Add(txtItemTitle.Text.Trim());
+
+            txtItemTitle.Focus();
+            txtItemTitle.SelectAll();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedIndex == -1)
+            {
+                labelSelectedItem1.Text = "Select an item";
+                btnDeleteItem.Enabled = false;
+                return;
+            }
+
+            labelSelectedItem1.Text = listBox1.SelectedItem.ToString();
+            btnDeleteItem.Enabled = true;
+        }
+
+        private void btnDeleteItem_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+                return;
+
+            ListBox.SelectedObjectCollection items = new ListBox.SelectedObjectCollection(listBox1);
+
+            for (int i = items.Count-1; i >= 0; i--)
+            {
+                listBox1.Items.Remove(items[i]);
+            }
+
+            //listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Animal> list = new List<Animal>();
+
+            list.Add(new Animal() { id = 1, type = "Cow", fullName = "Sarıkız" });
+            list.Add(new Animal() { id = 2, type = "Cow", fullName = "Benekli" });
+            list.Add(new Animal() { id = 3, type = "Cat", fullName = "Tekir" });
+            list.Add(new Animal() { id = 4, type = "Horse", fullName = "AliRıza Bey" });
+            list.Add(new Animal() { id = 5, type = "Cat", fullName = "Pamuk" });
+
+            listBox2.DataSource = list;
+            FillListBox(listBox2, list);
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedIndex == -1)
+            {
+                labelSelectedItem2.Text = "Select an item";
+                return;
+            }
+
+            //Animal item = listBox2.SelectedItem as Animal;
+            //if (item == null)
+            //{
+            //    labelSelectedItem2.Text = "Select an item";
+            //    return;
+            //}
+
+            string itemString = "";
+            foreach (Animal item in listBox2.SelectedItems)
+            {
+                itemString += item.fullName + " (" + item.type + ") " + Environment.NewLine;
+            }
+
+            labelSelectedItem2.Text = itemString;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedIndex == -1)
+            {
+                return;
+            }
+
+            var list = listBox2.DataSource as List<Animal>;
+            list.Remove((Animal)listBox2.SelectedItem);
+
+            FillListBox(listBox2, list);
+        }
+
+        private void FillListBox(ListBox listBox, List<Animal> list)
+        {
+            listBox2.DataSource = null;
+            listBox2.DataSource = list;
+            listBox2.DisplayMember = "fullName";
+            listBox2.ValueMember = "id";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == -1)
+                return;
+
+
+            Bitmap bitmap = new Bitmap(160, 160);
+            Graphics g = Graphics.FromImage(bitmap);
+            g.Clear(Color.White);
+
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0: //DrawCircle
+                    g.DrawEllipse(Pens.DarkMagenta, 5, 5, 150, 150);
+                    g.DrawString("X", new Font("Courier New", 40), Brushes.DarkMagenta, 55, 55);
+                    break;
+                case 1: //FillCircle
+                    g.FillEllipse(Brushes.Magenta, 5, 5, 150, 150);
+                    g.DrawString("O", new Font("Courier New", 40), Brushes.DarkMagenta, 55, 55);
+                    break;
+                default:
+                    break;
+            }
+
+            pictureBox1.Image = bitmap;
+        }
+    }
+}
